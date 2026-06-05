92ac81a32ed7a02793f8ab77699f730522989339
hoangtuan2k5 <hoangchieunguyentuan@gmail.com>
2026-06-05 20:36:43 +0700

    fix(backend): align bootstrap setup with SF-20 spec
    SF-20 #in-progress

--- backend/src/main/java/dev/hoangtuan/shopflow/ShopflowApplication.java
+++ backend/src/main/java/dev/hoangtuan/shopflow/ShopflowApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopflowBackendApplication {
+public class ShopflowApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ShopflowBackendApplication.class, args);
+    SpringApplication.run(ShopflowApplication.class, args);
   }
 }
--- backend/src/test/java/dev/hoangtuan/shopflow/ShopflowApplicationTests.java
+++ backend/src/test/java/dev/hoangtuan/shopflow/ShopflowApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
-class ShopflowBackendApplicationTests {
+class ShopflowApplicationTests {
 
   @Test
   void contextLoads() {}
