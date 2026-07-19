a901aec85cb080317963e798eace9a1a6f61dfbc
Hoàng Chiều Nguyễn Tuấn <tuan@hoangtuan.dev>
2026-07-20 01:46:25 +0700

    feat(order): define order data model (#38)
    SF-12

---
+++ backend/src/main/java/dev/hoangtuan/shopflow/order/OrderStatus.java
@@ -0,0 +1,8 @@
+package dev.hoangtuan.shopflow.order;
+
+enum OrderStatus {
+  PENDING_PAYMENT,
+  PAID,
+  PAYMENT_FAILED,
+  CANCELLED
+}
---
+++ backend/src/main/java/dev/hoangtuan/shopflow/order/PaymentMethod.java
@@ -0,0 +1,6 @@
+package dev.hoangtuan.shopflow.order;
+
+enum PaymentMethod {
+  CARD,
+  COD
+}
